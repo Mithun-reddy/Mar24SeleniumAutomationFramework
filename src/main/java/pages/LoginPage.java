030573bce0d73781b4fb7c9dc2722085dd5ed24e
Mithun Reddy <user@Mithuns-Mac.local>
2024-05-01 15:37:11 +0530

    Framework structure

--- src/main/java/pages/LoginPage.java
+++ src/main/java/pages/LoginPage.java
@@ -1,5 +1,36 @@
 package pages;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
 public class LoginPage {
+	
+	public LoginPage(WebDriver driver) {
+		
+		PageFactory.initElements(driver, this);
+	
+	}
+	
+	
+	
+	
+	@FindBy(id = "useremail")
+	public WebElement username;
+	
+	@FindBy(id = "password")
+	public WebElement password;
+	
+	@FindBy(id = "login")
+	public WebElement loginButton;
+	
+	public void loginToSFDC(String userEmail, String passWord) {
+		this.username.sendKeys(userEmail);
+		this.password.sendKeys(passWord);
+		this.loginButton.click();
+	}
+	
+	
 
 }
--- src/main/java/tests/LoginTest.java
+++ src/main/java/tests/LoginTest.java
@@ -1,7 +1,22 @@
 package tests;
 
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+import pages.LoginPage;
 
 public class LoginTest extends BaseTest {
+	
+	@Test
+	public void firstTest() {
+		WebDriver driver = BaseTest.getDriver("chrome", false);
+		LoginPage lp = new LoginPage(driver);
+		driver.get("");
+//		lp.username.sendKeys("");
+//		lp.password.sendKeys("");
+//		lp.loginButton.click();
+		lp.loginToSFDC("", "");
+	}
 	
 	
 }
